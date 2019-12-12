adaa246ebc69b77ef8ccf43f8d8bad449325db22
foo <foo@foo.com>
2019-12-12 09:13:14 -0600

    quickPush

--- source/comp/AppChat/AppChat.jsx
+++ source/comp/AppChat/AppChat.jsx
@@ -45,3 +45,26 @@ class AppChat extends React.Component {
 }
 
 export default connect()(AppChat);
+
+// // Make Connection
+// var socket = io.connect('http://localhost:4000');
+
+// // DOM
+// var message = document.getElementById('message');
+// var handle = document.getElementById('handle');
+// var btn = document.getElementById('send');
+// var output = document.getElementById('output');
+// var feedback = document.getElementById('feedback');
+
+// btn.addEventListener('click', function(){
+//   socket.emit('chat', {
+//     message: message.value,
+//     handle: handle.value
+//   });
+//   message.value = "";
+// });
+
+// socket.on('chat', function(data){
+//   feedback.innerHTML = '';
+//   output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+// });
