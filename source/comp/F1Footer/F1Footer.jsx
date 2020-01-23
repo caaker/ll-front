16ce35771bf80adac3b592b16cfb7213cff25cdf
foo <foo@foo.com>
2020-01-23 17:44:22 -0600

    quickPush

--- source/comp/F1Footer/F1Footer.jsx
+++ source/comp/F1Footer/F1Footer.jsx
@@ -11,7 +11,7 @@ class F1Footer extends React.Component {
     return (
       
       <div class="footer">
-        <p class='contact'>contact@livelong.ai</p>
+        <p class='contact'>chris@livelong.ai</p>
       </div>
 
     )
