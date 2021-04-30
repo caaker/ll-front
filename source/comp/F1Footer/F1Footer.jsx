51fac6dcb58c1b7dc6317e4c1d28f3f5d9d35bea
C <c@Cs-MacBook-Air.local>
2021-04-29 20:29:03 -0500

    quickPush

--- source/comp/F1Footer/F1Footer.jsx
+++ source/comp/F1Footer/F1Footer.jsx
@@ -14,7 +14,6 @@ class F1Footer extends React.Component {
         <a className="contact" href="mailto:chris@livelong.ai?subject='front page inquiry'">contact</a>
         <a className="contact" target="_blank" href="https://www.lucidchart.com/documents/view/1a04a57f-a2ee-4cdc-a75a-bbff8dbbd4d9/0_0">lucid</a>
         <a className="contact" target="_blank" href="https://repl.it/@livelong1">replits</a>
-        <a className="contact" target="_blank" href="https://www.pinterest.com/livelongdotai/">pinterest</a>
       </div>
 
     )
@@ -23,6 +22,8 @@ class F1Footer extends React.Component {
 export default F1Footer;
 
 /* 
+
+<a className="contact" target="_blank" href="https://www.pinterest.com/livelongdotai/">pinterest</a>
 
 https://repl.it/@livelong1 
 
